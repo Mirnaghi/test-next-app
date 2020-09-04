e8ae1288b3e5a3cdf82b10f2511c9cd74e201186
Admin <mirnaghi7175@gmail.com>
2020-09-04 16:41:07 +0400

    create basic project structure

---
+++ src/containers/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MenuBar from '../../components/MenuBar/MenuBar';
+
+function App() {
+  return (
+    <div>
+     
+    
+    </div>
+  );
+}
+
+export default App;
