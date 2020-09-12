f690942fed977ab2fad75ae30c5b11c163cf1670
VuqarTagiyev <vuqartagiyeff@mail.ru>
2020-09-12 22:38:44 +0400

    SearchBar and TopBar

---
+++ src/components/TopBar/TopBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TopBar.css';
+
+const TopBar = () => {
+    return (
+        <h1 className = "top-bar">
+            Xəyalınızdakı ixtisasa yiyələnmək üçün ən rahat yol
+        </h1>
+    )
+}
+
+export default TopBar;
