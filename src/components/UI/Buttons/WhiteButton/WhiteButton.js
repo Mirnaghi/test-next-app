f228e607204610bf8b009c1f44da1728ea2292a5
Admin <mirnaghi7175@gmail.com>
2020-09-21 11:08:18 +0400

    make modifications to  WhiteButton

--- src/components/UI/Buttons/WhiteButton/WhiteButton.js
+++ src/components/UI/Buttons/WhiteButton/WhiteButton.js
@@ -3,7 +3,7 @@ import "./WhiteButton.css";
 import { buttonFontStyle } from '../../../../constants';
 
 const WhiteButton = (props) => {
-  return <button style={buttonFontStyle} className="WhiteButton">{props.name}</button>;
+  return <button style={buttonFontStyle} className={props.border ? 'WhiteButton withBorder': 'WhiteButton'}>{props.name}</button>;
 };
 
 export default WhiteButton;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,14 +2,29 @@ import React from 'react';
 import TopBar from '../../components/TopBar/TopBar';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import './Home.css'
+import {useState} from 'react';
+import SearchResult from '../../containers/SearchResult/SearchResult';
 
 const Home = (props) => {
+    const [userInput, setUserInput] = useState('hdchg');
+    
     return (
-        <div className="container">
-            <TopBar />
-            <SearchBar />
-        </div>
+        
+            <div className="home__container">
+                {userInput === '' ? 
+                    <React.Fragment>
+                        <TopBar/> 
+                        <SearchBar />
+                    </React.Fragment> 
+                : <React.Fragment>
+                    <SearchBar/> 
+                    <SearchResult/>
+                  </React.Fragment>}
+            </div>
+        
     );
+    
+
 }
 
 export default Home;
