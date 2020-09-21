ec2fb32df54224681844d7477de3c3d64d79e783
Admin <mirnaghi7175@gmail.com>
2020-09-21 11:09:42 +0400

    add fontawesome icons

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import { faCheckCircle, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import './fontawesome';
+
 export const buttonFontStyle = {
     "font-family": "Roboto",
     "font-style": "normal",
@@ -6,3 +11,12 @@ export const buttonFontStyle = {
     "line-height": 16,
     "letter-spacing": "0.05em"
 };
+
+
+// fontawesome icons 
+export const availableIcon = <FontAwesomeIcon 
+                                    icon={faCheckCircle} 
+                                    style={{color: '#2BC44D', width: 18, height: 18}}/>;
+export const unavailableIcon = <FontAwesomeIcon 
+                                    icon={faTimesCircle} 
+                                    style={{color: '#E7574F', width: 18, height: 18}}/>;
