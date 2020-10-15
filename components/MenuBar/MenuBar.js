036a2e7e10938bc81d826d3e27cc69883cb4e629
Admin <mirnaghi7175@gmail.com>
2020-10-15 16:49:07 +0400

    make MenuBar mobile responsive

--- components/MenuBar/MenuBar.js
+++ components/MenuBar/MenuBar.js
@@ -1,28 +1,44 @@
 import Link from 'next/link'
-import React from 'react';
-import styles from './MenuBar.module.css';
-// import logo from '../../public/logo.png'
-// import searchIcon from '../../public/search-icon.png';
-import SignInButton from '../UI/Buttons/SignInButton/SignInButton';
-import WhiteButton from '../UI/Buttons/WhiteButton/WhiteButton';
+import React from 'react'
+import { useState } from 'react'
+import styles from './MenuBar.module.css'
+
+import SignInButton from '../UI/Buttons/SignInButton/SignInButton'
+import WhiteButton from '../UI/Buttons/WhiteButton/WhiteButton'
+
+import { hamburgerIcon, closeMenuIcon} from '../../constants'
 
 const MenuBar = (props) => {
+
+    const [clicked, setClicked] = useState(false);
+
+    const handleClick = () => {
+        setClicked(!clicked);
+    }
+
     return (
         <header className={styles.menuBar}>
             <Link className={styles.logo} href="/"><img className={styles.logo} src="/logo.png" alt="logo"/></Link>
+            <div className={styles.icons__for__mobile} onClick={handleClick}>
+                {clicked ? closeMenuIcon : hamburgerIcon}
+            </div>
+            
             <nav>
-                <ul className={styles.nav__links}>
+                <ul className={clicked ? styles.nav__links_active : styles.nav__links}>
                     <li className={styles.nav__link}><Link href='/about'>Haqqımızda</Link></li>
                     <li className={styles.nav__link}><Link href='/how-works'>Necə işləyirik</Link></li>
                     <li className={styles.nav__link}><Link href='/specialties'>İxtisaslar</Link></li>
                     <li className={styles.nav__link}><Link href='/wishes'>istəklər</Link></li>
+
+                    <div className={styles.buttons__container}>
+                        <a className={styles.button}><WhiteButton name="DAXIL OL"/></a>
+                        <a className={styles.button}><SignInButton /></a>
+                    </div>
                 </ul>
             </nav>
-            <div>
-                <a className="button"><WhiteButton name="DAXIL OL"/></a>
-                <a className="button"><SignInButton /></a>
-            </div>
-            <img className={styles.search_icon} src="/search-icon.png" alt="s"/>
+            
+            
+            <img className={styles.search__icon} src="/search-icon.png" alt="s"/>
         </header>
     )
 }
--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { faCheckCircle, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import { faCheckCircle, faTimesCircle, faUser, faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import './fontawesome';
 
@@ -14,9 +14,26 @@ export const buttonFontStyle = {
 
 
 // fontawesome icons 
+
+// icons for MenuBar component
+export const hamburgerIcon = <FontAwesomeIcon 
+                                    icon={faBars}
+                                    style={{color: '#FFFFFF', width: 32, height: 32}}/>
+
+export const closeMenuIcon = <FontAwesomeIcon 
+                                    icon={faTimes}
+                                    style={{color: '#FFFFFF', width: 32, height: 32}}/>                                    
+
+// icons for FoundProfessionCard component
 export const availableIcon = <FontAwesomeIcon 
                                     icon={faCheckCircle} 
                                     style={{color: '#2BC44D', width: 18, height: 18}}/>;
 export const unavailableIcon = <FontAwesomeIcon 
                                     icon={faTimesCircle} 
                                     style={{color: '#E7574F', width: 18, height: 18}}/>;
+
+
+// icon for ModuleCard component                                    
+export const userIconModule = <FontAwesomeIcon 
+                                    icon={faUser}
+                                    style={{color: '#C4C4C4'}}/>
