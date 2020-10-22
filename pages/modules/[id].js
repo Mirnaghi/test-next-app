a13d5b8031524f02e871885b0159072260f15624
Admin <mirnaghi7175@gmail.com>
2020-10-22 21:35:12 +0400

    add components to module page and style them

--- pages/modules/[id].js
+++ pages/modules/[id].js
@@ -2,8 +2,12 @@ import MenuBar from '../../components/MenuBar/MenuBar'
 import RequirementsCard from '../../components/RequirementsCard/RequirementsCard'
 import SupervisorsField from '../../containers/SupervisorsField/SupervisorsField'
 import LessonCard from '../../components/LessonCard/LessonCard'
+import DropdownButton from '../../components/DropdownButton/DropdownButton'
+import BlueButton from '../../components/UI/Buttons/BlueButton/BlueButton'
 
 import styles from '../../styles/Modules.module.css'
+
+import { userIconModule } from '../../constants'
 
 export default function Modules({ lesson }){
     
@@ -13,16 +17,52 @@ export default function Modules({ lesson }){
                 <MenuBar />
             </div>
             <div className={styles.modules__container}>
+                <section className={styles.main__section}>
+                    <section className={styles.head__section}>
+
+                        <div class={styles.enrollment}>
+                            <div className={`${styles.icon}`}>{userIconModule}</div>
+                            <p className={styles.enrollment__p}>255 nəfər</p>
+                        </div>
+
+                        <div class={styles.main}>
+                            <h2 className={styles.module__title}>HTML basic understanding</h2>
+                            <p className={styles.main__p}>
+                            Lorem ipsum, tenetur illum quasi reprehenderit neque atque magni quod.Numquam explicbo exercitationem earum reprehenderit.Dignissimos, nemo dolor?
+                            </p>
+                            <div class={styles.detail}>
+                                <div className={styles.detail__p}> 
+                                    <span> {module.lessons} dərs |</span> 
+                                    <span> Video materiallar |</span> 
+                                    <span> Xüsusi kontent</span>
+                                </div>
+                    
+                            </div>
+                        </div>
+
+                    </section>
+                    <section className={styles.offer__section}>
+                        <p>Bu modula aid menbeler</p>
+                        <BlueButton name="Mənbə təklif et"/>
+                    </section>
+                <section className={styles.filter__section}>
+                    <DropdownButton name="Status" values={['Mutleq', 'Orta', 'Vacib']}/>
+                    <DropdownButton name="Dil secimi" values={['Mutleq', 'Orta', 'Vacib']}/>
+                    <DropdownButton name="Vaciblik" values={['Mutleq', 'Orta', 'Vacib']}/>
+                    <DropdownButton name="Tipi" values={['Mutleq', 'Orta', 'Vacib']}/>
+                    <DropdownButton name="Rate" values={['Mutleq', 'Orta', 'Vacib']}/>
+                    <DropdownButton name="Qiymet" values={['Mutleq', 'Orta', 'Vacib']}/>
+                </section>
                 <section className={styles.lessons__section}>
                     <div className={styles.filter__names}>
                         <p className={styles.filter__name__head}>Status / Adi</p>  
-                    <div className={styles.filter__names__inner}>      
-                        <p className={styles.filter__name}>Dil secimi</p>
-                        <p className={styles.filter__name}>Vacib</p>
-                        <p className={styles.filter__name}>Tipi</p>
-                        <p className={styles.filter__name}>Rate</p>
-                        <p className={styles.filter__name}>Qiymeti</p>
-                    </div>    
+                        <div className={styles.filter__names__inner}>      
+                            <p className={styles.filter__name}>Dil secimi</p>
+                            <p className={styles.filter__name}>Vacib</p>
+                            <p className={styles.filter__name}>Tipi</p>
+                            <p className={styles.filter__name}>Rate</p>
+                            <p className={styles.filter__name}>Qiymeti</p>
+                        </div>    
                     </div>
                     <LessonCard />
                     <LessonCard />
@@ -30,6 +70,8 @@ export default function Modules({ lesson }){
                     <LessonCard />
 
                 </section>
+                </section>
+                
                 <section className={styles.detail__section}>
                     <RequirementsCard />
                     <SupervisorsField />
@@ -39,6 +81,7 @@ export default function Modules({ lesson }){
 
     );
 }
+
 
 export async function getStaticPaths(){
     const paths = [{ params: {id: 'html'}}]
