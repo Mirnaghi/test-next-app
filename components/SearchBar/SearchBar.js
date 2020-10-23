98b769ff15dafb5ccafb538b7f544bf9aa89a806
Admin <mirnaghi7175@gmail.com>
2020-10-23 22:12:24 +0400

    solve responsiveness issues

--- components/SearchBar/SearchBar.js
+++ components/SearchBar/SearchBar.js
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from './SearchBar.module.css';
 
-const SearchBar = () => {
+const SearchBar = ({ handleChange, handleSubmit, value }) => {
     return (
         <div className = {styles.search__bar}>
-            <input className = {styles.search__bar__input} placeholder = "Xəyalndakı ixtisası daxil et (Məs: Dizayner, Developer və s)"/>
-            <button className = {styles.search__bar__button}>
+            <input onChange={handleChange} value={value} name="search_input" className = {styles.search__bar__input} placeholder = "Xəyalndakı ixtisası daxil et (Məs: Dizayner, Developer və s)"/>
+            <button onClick={handleSubmit} className = {styles.search__bar__button}>
                 AXTAR
             </button>
         </div>
