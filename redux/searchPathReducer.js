1100ee4c969a052715f382f73c5b250537dd6443
Admin <mirnaghi7175@gmail.com>
2020-10-25 22:00:28 +0400

    add searchPath action creator and reducer

---
+++ redux/searchPathReducer.js
@@ -0,0 +1,23 @@
+import {getPathes} from '../services/pathServices'
+
+export function searchPath(pathName) {
+    return async (dispatch) => {
+            const pathes = await getPathes()
+            const foundPathes = pathes.filter(path => { 
+                return path.name.toLowerCase().includes(pathName.toLowerCase())
+            })
+            dispatch({ type: "SEARCH_PATH", payload: foundPathes})
+            console.log("Result of bu", foundPathes)
+    }
+}
+
+function searchPathReducer(foundPathes = [], action) {
+    switch(action.type) {
+        case "SEARCH_PATH":
+            return action.payload
+        default:
+            return foundPathes    
+    }
+}
+
+export default searchPathReducer;
