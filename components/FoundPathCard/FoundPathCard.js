5831dd06a2e547725d7b5e9ea2366bbe6d3df96f
Admin <mirnaghi7175@gmail.com>
2020-10-25 21:56:25 +0400

    modify Components for search path functionality

---
+++ components/FoundPathCard/FoundPathCard.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styles from './FoundPathCard.module.css';
+import WhiteButton from '../UI/Buttons/WhiteButton/WhiteButton'
+import { availableIcon, unavailableIcon } from '../../constants';
+
+
+const FoundPathCard = ({path}) => {
+    return  (
+        <div class={styles.card}>
+            <section class={styles.card__head}>
+                <h2>{path.name}</h2>
+                <p>{path.studentCount} istifadəçi</p>
+            </section>
+            <section class={styles.detail__section}>
+                <div class={styles.available__content}>
+                    <p>
+                        {path.free_cont ? availableIcon: unavailableIcon} 
+                        &nbsp; &nbsp; Ödənişsiz mənbələr
+                    </p>
+                    <p>
+                        {path.foreign_resources.length > 0 ? availableIcon: unavailableIcon} 
+                        &nbsp; &nbsp; Xüsusi kontent
+                    </p>
+                </div>
+                <div class={styles.list__head}>Xarici mənbələr:</div>
+                <ul class={styles.foreign__resources}>
+                    {path.foreign_resources.map(resource => {
+                        return (
+                        <li key={path.foreign_resources.indexOf(resource)} class={styles.foreign__resource}>
+                            {resource}
+                        </li>);
+                    })}
+                </ul>
+            </section>
+
+            <section class={styles.card__footer}>
+                <a className={styles.requirements}>Tələblər</a>
+                <WhiteButton border={true} name="Başla"/>
+            </section>
+        </div>
+    );
+}
+
+export default FoundPathCard;
--- containers/SearchResult/SearchResult.js
+++ containers/SearchResult/SearchResult.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import styles from './SearchResult.module.css';
-import FoundProfessionCard from '../../components/FoundProfessionCard/FoundProfessionCard';
+import FoundPathCard from '../../components/FoundPathCard/FoundPathCard';
 
 const SearchResult = ({searchResult}) => {
     return (
         <div class={styles.result__container}>
-            <FoundProfessionCard foreignResource={true} freeContent={false}/>
-            <FoundProfessionCard foreignResource={true} freeContent={true}/>
-            <FoundProfessionCard foreignResource={true} freeContent={true}/>
-            <FoundProfessionCard foreignResource={false} freeContent={true}/>
-            <FoundProfessionCard foreignResource={true} freeContent={true}/>
-            <FoundProfessionCard foreignResource={false} freeContent={true}/>
-            <FoundProfessionCard foreignResource={true} freeContent={true}/>
-            <FoundProfessionCard foreignResource={true} freeContent={true}/>
+            {searchResult.map(path => {
+                return <FoundPathCard path={path}/>
+            })}
         </div>
     );
 }
