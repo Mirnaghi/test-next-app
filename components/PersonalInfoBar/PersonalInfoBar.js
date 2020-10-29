3cc82207bb3613f7258c2807505f19060e241a5f
Admin <mirnaghi7175@gmail.com>
2020-10-29 21:34:03 +0400

    create and style PersonalInfoBar component

---
+++ components/PersonalInfoBar/PersonalInfoBar.js
@@ -0,0 +1,25 @@
+import styles from './PersonalInfoBar.module.css'
+
+function PersonalInfoBar({ user }){
+    return (
+        <div className={styles.container}>
+            
+                <img className={styles.photo} src={user.img}/>
+          
+            <div className={styles.input__container}>
+                <label for="fname">Ad, soyad</label>
+                <input id="fname" className={styles.input__field} type="text" name="fullname" value={user.fullname}/>
+            </div>
+            <div className={styles.input__container}>
+                <label for="phone">Telefon</label>
+                <input id="phone" className={styles.input__field} type="text" name="phoneNumber" value={user.phone}/>
+            </div>
+            <div className={styles.input__container_lg}>
+                <label for="email">E-poçt ünvanı</label>
+                <input id="email" className={styles.input__field} type="email" name="email" value={user.email}/>
+            </div>
+        </div>
+    );
+}
+
+export default PersonalInfoBar
