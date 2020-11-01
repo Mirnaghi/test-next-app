d4c6c92f87b0719179c3cceea0745869959b7827
Admin <mirnaghi7175@gmail.com>
2020-11-01 04:29:00 +0400

    center SignInField

--- containers/SignUpField/SignUpField.js
+++ containers/SignUpField/SignUpField.js
@@ -18,19 +18,16 @@ function SignUpField(){
                     <input id="fullname" className={styles.long__input} name="fullname" placeholder="Ad Soyad"></input>
                     <label for="fullname">E-poçt ünvanı</label>
                     <input className={styles.long__input} name="email" placeholder="AdSoyad@example.com"></input>
-                    
-                    
-                    <div className={styles.password__container}>
-                        
+        
+                    <div className={styles.password__container}>  
                        <div className={styles.password}>
                             <label for="psswd">Parol</label>
-                            <input  id="psswd" className={styles.password__input} type="password" name="phoneNumber" placeholder="password"></input>
+                            <input  id="psswd" className={styles.password__input} type="password" name="password" placeholder="password"></input>
                         </div> 
                         <div className={styles.password}>
                             <label for="psswd-confirm">Parolu tesdiq et</label>
-                            <input id="psswd-confirm" className={styles.password__input} type="password" name="phoneNumber" placeholder="password"></input>
+                            <input id="psswd-confirm" className={styles.password__input} type="password" name="password_confirm" placeholder="password"></input>
                         </div>
-                         
                     </div>
                     <label for="fullname">Telefon nömrəsi</label>
                     <input className={styles.short__input} name="phoneNumber" placeholder="(___) ___ __ __"></input>
