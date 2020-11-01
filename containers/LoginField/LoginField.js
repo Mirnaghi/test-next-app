c6b87f4cd811784daa7fb80bdc8457dcfc45a8d5
Admin <mirnaghi7175@gmail.com>
2020-11-01 04:30:08 +0400

    create and style LoginField container component

--- containers/LoginField/LoginField.js
+++ containers/LoginField/LoginField.js
@@ -2,6 +2,7 @@ import Modal from 'react-modal'
 import {useDispatch, useSelector} from 'react-redux'
 import {openLoginModal} from '../../redux/authModalReducer'
 import styles from './LoginField.module.css'
+import SignInButton from '../../components/UI/Buttons/SignInButton/SignInButton'
 
 function LoginField(){
 
@@ -11,7 +12,18 @@ function LoginField(){
     return (
         <Modal className={styles.modal__style} isOpen={loginIsOpen} onRequestClose={() => dispatch(openLoginModal(false))}>
             <div className={styles.login__body}>
-            <h2>Login field in here</h2>
+                <h4 className={styles.login__head}>Daxil ol</h4>
+                <form className={styles.login__form} onSubmit={() => console.log("Login form submitted")}>
+                    <div>
+                        <label for="email">E-poçt ünvanı</label>
+                        <input id="email" className={styles.long__input} name="email" placeholder="AdSoyad@example.com"></input>
+                    </div> 
+                    <div className={styles.input__wrapper}>
+                        <label for="password">Parol</label>
+                        <input  id="password" className={styles.password__input} type="password" name="password" placeholder="password"></input>
+                    </div>
+                </form>
+                <div className={styles.login__button}><SignInButton name="Daxil ol" /></div>
             </div>
         </Modal>
     )
