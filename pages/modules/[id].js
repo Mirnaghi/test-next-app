a24bf96af7aea53e80428c8c9edfe15899856535
Admin <mirnaghi7175@gmail.com>
2020-11-02 21:17:47 +0400

    modify modules page to fit Lessons table

--- pages/modules/[id].js
+++ pages/modules/[id].js
@@ -9,8 +9,8 @@ import styles from '../../styles/Modules.module.css'
 
 import { userIconModule } from '../../constants'
 
-export default function Modules({ lesson }){
-    
+export default function Modules({ lesson }) {
+
     return (
         <div>
             <header>
@@ -28,50 +28,50 @@ export default function Modules({ lesson }){
                         <div class={styles.main}>
                             <h2 className={styles.module__title}>HTML basic understanding</h2>
                             <p className={styles.main__p}>
-                            Lorem ipsum, tenetur illum quasi reprehenderit neque atque magni quod.Numquam explicbo exercitationem earum reprehenderit.Dignissimos, nemo dolor?
+                                Lorem ipsum, tenetur illum quasi reprehenderit neque atque magni quod.Numquam explicbo exercitationem earum reprehenderit.Dignissimos, nemo dolor?
                             </p>
                             <div class={styles.detail}>
-                                <div className={styles.detail__p}> 
-                                    <span> {module.lessons} dərs |</span> 
-                                    <span> Video materiallar |</span> 
+                                <div className={styles.detail__p}>
+                                    <span> {module.lessons} dərs |</span>
+                                    <span> Video materiallar |</span>
                                     <span> Xüsusi kontent</span>
                                 </div>
-                    
+
                             </div>
                         </div>
 
                     </section>
                     <section className={styles.offer__section}>
                         <p>Bu modula aid menbeler</p>
-                        <BlueButton name="Mənbə təklif et"/>
+                        <BlueButton name="Mənbə təklif et" />
                     </section>
-                <section className={styles.filter__section}>
-                    <DropdownButton name="Status" values={['Mutleq', 'Orta', 'Vacib']}/>
-                    <DropdownButton name="Dil secimi" values={['Mutleq', 'Orta', 'Vacib']}/>
-                    <DropdownButton name="Vaciblik" values={['Mutleq', 'Orta', 'Vacib']}/>
-                    <DropdownButton name="Tipi" values={['Mutleq', 'Orta', 'Vacib']}/>
-                    <DropdownButton name="Rate" values={['Mutleq', 'Orta', 'Vacib']}/>
-                    <DropdownButton name="Qiymet" values={['Mutleq', 'Orta', 'Vacib']}/>
+                    <section className={styles.filter__section}>
+                        <DropdownButton name="Status" values={['Mutleq', 'Orta', 'Vacib']} />
+                        <DropdownButton name="Dil secimi" values={['Mutleq', 'Orta', 'Vacib']} />
+                        <DropdownButton name="Vaciblik" values={['Mutleq', 'Orta', 'Vacib']} />
+                        <DropdownButton name="Tipi" values={['Mutleq', 'Orta', 'Vacib']} />
+                        <DropdownButton name="Rate" values={['Mutleq', 'Orta', 'Vacib']} />
+                        <DropdownButton name="Qiymet" values={['Mutleq', 'Orta', 'Vacib']} />
+                    </section>
+                    <section className={styles.lessons__section}>
+                        <table className={styles.lessons}>
+                            <tr class="lesson__head">
+                                <th class="filter__keyword">Status / Adi</th>
+                                <th class="filter__keyword">Dil secimi</th>
+                                <th class="filter__keyword">Vacib</th>
+                                <th class="filter__keyword">Tipi</th>
+                                <th class="filter__keyword">Rate</th>
+                                <th class="filter__keyword">Qiymeti</th>
+                                <th class="filter__keyword"></th>
+                            </tr>
+                            <LessonCard />
+                            <LessonCard />
+                            <LessonCard />
+                            <LessonCard />
+                        </table>
+                    </section>
                 </section>
-                <section className={styles.lessons__section}>
-                    <div className={styles.filter__names}>
-                        <p className={styles.filter__name__head}>Status / Adi</p>  
-                        <div className={styles.filter__names__inner}>      
-                            <p className={styles.filter__name}>Dil secimi</p>
-                            <p className={styles.filter__name}>Vacib</p>
-                            <p className={styles.filter__name}>Tipi</p>
-                            <p className={styles.filter__name}>Rate</p>
-                            <p className={styles.filter__name}>Qiymeti</p>
-                        </div>    
-                    </div>
-                    <LessonCard />
-                    <LessonCard />
-                    <LessonCard />
-                    <LessonCard />
 
-                </section>
-                </section>
-                
                 <section className={styles.detail__section}>
                     <RequirementsCard />
                     <SupervisorsField />
@@ -83,8 +83,8 @@ export default function Modules({ lesson }){
 }
 
 
-export async function getStaticPaths(){
-    const paths = [{ params: {id: 'html'}}]
+export async function getStaticPaths() {
+    const paths = [{ params: { id: 'html' } }]
 
     return {
         paths,
@@ -93,23 +93,23 @@ export async function getStaticPaths(){
 
 }
 
-export async function getStaticProps({ params }){
+export async function getStaticProps({ params }) {
     const lessons = [{
-            name: "HTML nedir?",
-            language: 'AZE',
-            importance: 'Mutleq',
-            type: 'video',
-            price: 'free',
-            rating: 4.5, 
-        },
-        {
-            name: "HTML nedir?",
-            language: 'AZE',
-            importance: 'Mutleq',
-            type: 'video',
-            price: 'free',
-            rating: 4.5, 
-        }
+        name: "HTML nedir?",
+        language: 'AZE',
+        importance: 'Mutleq',
+        type: 'video',
+        price: 'free',
+        rating: 4.5,
+    },
+    {
+        name: "HTML nedir?",
+        language: 'AZE',
+        importance: 'Mutleq',
+        type: 'video',
+        price: 'free',
+        rating: 4.5,
+    }
     ]
 
     return {
