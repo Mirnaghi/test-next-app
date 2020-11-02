23cd77244628f4d69388cf5cb1df130462fc579f
Admin <mirnaghi7175@gmail.com>
2020-11-02 21:16:40 +0400

    modify LessonCard to fit in Lessons table

--- components/LessonCard/LessonCard.js
+++ components/LessonCard/LessonCard.js
@@ -3,7 +3,25 @@ import { greenIcon, yellowIcon } from '../../constants';
 
 export default function LessonCard({ lesson }){
     return (
-        <div className={styles.lesson__card}>
+        <tr className={styles.lesson__row}>
+        <td className={styles.lesson__name}>
+          <i className={styles.head__icon}>{greenIcon }</i>
+            What is html
+        </td>
+        <td className={styles.lesson + " " + styles.lesson__lang}>Aze</td>
+        <td className={styles.lesson + " " + styles.lesson__imp}>Mutleq</td>
+        <td className={styles.lesson + " " + styles.lesson__type}>Video</td>
+        <td className={styles.lesson + " " + styles.lesson__rate}>4.5 / 5</td>
+        <td className={styles.lesson + " " + styles.lesson__price}>Odenissiz</td>
+        <td className={styles.lesson}><a href="#" className={styles.start__button}>Basla</a></td>
+      </tr>
+    );
+}
+
+       
+
+
+{/* <div className={styles.lesson__card}>
             <secton className={styles.head__section}>
                 <i className={styles.head__icon}>{greenIcon }</i>
                 <p className={styles.lesson__name}> What is HTML</p>
@@ -16,6 +34,4 @@ export default function LessonCard({ lesson }){
                 <p className={styles.lesson__price}>odenissiz</p>
                 <a className={styles.start__button}>Basla</a>
             </section>
-        </div>
-    );
-}
+        </div> */}
