0ce09c99e2ab24fdd8a9704be8ec43f0560b71c6
Admin <mirnaghi7175@gmail.com>
2020-11-09 21:10:35 +0400

    modify SideBar and SideBarButton

--- components/UI/Buttons/SideBarButton/SideBarButton.js
+++ components/UI/Buttons/SideBarButton/SideBarButton.js
@@ -2,9 +2,9 @@ import React from "react";
 import styles from "./SideBarButton.module.css";
 
 const SideBarButton = ({name, handleClick}) => {
- 
+
   return <button className={styles.SideBarButton} onClick={handleClick}>{name}</button>;
-  
+
 };
 
 export default SideBarButton;
--- containers/SideBar/SideBar.js
+++ containers/SideBar/SideBar.js
@@ -1,13 +1,21 @@
+import {useDispatch} from 'react-redux'
+import {changeAccountPage} from '../../redux/accountPageReducer.js'
 import styles from './SideBar.module.css'
 import SideBarButton from '../../components/UI/Buttons/SideBarButton/SideBarButton'
 
 function SideBar() {
+
+    const dispatch = useDispatch()
+
     return (
         <div className={styles.sidebar__container}>
             <div className={styles.button__wrapper}>
-                <SideBarButton name="Öyrəndiyin ixtisaslar"/>
-                <SideBarButton name="Olmasını istədiyin ixtisas"/>
-                <SideBarButton name="Şəxsi məlumatlar"/>
+                <SideBarButton name="Öyrəndiyin ixtisaslar"
+                               handleClick={() => dispatch(changeAccountPage('learned_specialties'))}/>
+                <SideBarButton name="Olmasını istədiyin ixtisas"
+                               handleClick={() => dispatch(changeAccountPage('wished_specialties'))}/>
+                <SideBarButton name="Şəxsi məlumatlar"
+                               handleClick={() => dispatch(changeAccountPage('user_personal_info'))}/>
             </div>
         </div>
     )
