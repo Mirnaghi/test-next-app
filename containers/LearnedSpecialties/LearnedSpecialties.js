a07eaad3ac6e199cf12bade581bec64bec2cfa07
Admin <mirnaghi7175@gmail.com>
2020-11-09 21:29:21 +0400

    create and style LearnedSpecialties container component

---
+++ containers/LearnedSpecialties/LearnedSpecialties.js
@@ -0,0 +1,11 @@
+import styles from './LearnedSpecialties.module.css'
+
+function LearnedSpecialties() {
+    return (
+        <div>
+            Learned Specialties
+        </div>
+    );
+}
+
+export default LearnedSpecialties 
