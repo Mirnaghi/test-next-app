720f1f202fde4d89cac8735303012a34fcc96f91
Admin <mirnaghi7175@gmail.com>
2020-11-14 14:08:27 +0400

    add state for user authentication

--- redux/index.js
+++ redux/index.js
@@ -4,6 +4,7 @@ import thunk from "redux-thunk"
 import authModalReducer from "./authModalReducer"
 import menuBarReducer from "./menuBarReducer"
 import accountPageReducer from "./accountPageReducer"
+import userAuthReducer from "./userAuthReducer"
 // for debug
 import {searchPath} from './searchPathReducer'
 
@@ -14,7 +15,8 @@ const rootReducer = combineReducers({
     foundPathes: searchPathReducer,
     authModal: authModalReducer,
     menuBarIsOpen: menuBarReducer,
-    accountPage: accountPageReducer
+    accountPage: accountPageReducer,
+    userAuth: userAuthReducer
 })
 
 const store = createStore(rootReducer, applyMiddleware(thunk))
