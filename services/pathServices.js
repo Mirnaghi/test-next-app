2c4479858a7e506057f9c4539efd86ee09bff8f0
Admin <mirnaghi7175@gmail.com>
2020-11-14 14:09:24 +0400

    modify pathServices

--- services/pathServices.js
+++ services/pathServices.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3001/pathes';
+const baseURL = 'http://138.68.91.222';
 
 export const getPathes = async () => {
-    const response = await axios.get(baseURL).then(response => {
+    const response = await axios.get(`${baseURL}/api/pathes/all`).then(response => {
         return response;
     }).then(value => value)
     return response.data;
