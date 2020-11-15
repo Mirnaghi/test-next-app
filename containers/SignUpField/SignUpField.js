34d5e05610d6e0274320a66d462eed8c32de91c8
Admin <mirnaghi7175@gmail.com>
2020-11-15 21:27:07 +0400

    add validation to SignUpField and modify styling

--- containers/SignUpField/SignUpField.js
+++ containers/SignUpField/SignUpField.js
@@ -1,97 +1,140 @@
 import Modal from 'react-modal'
-import {useDispatch, useSelector} from 'react-redux'
-import {useForm} from 'react-hook-form'
-import {openSignUpModal, openLoginModal} from '../../redux/authModalReducer'
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useForm } from 'react-hook-form'
+import { openSignUpModal, openLoginModal } from '../../redux/authModalReducer'
 import { signInUser } from '../../redux/userAuthReducer'
 import styles from './SignUpField.module.css'
 import SignInButton from '../../components/UI/Buttons/SignInButton/SignInButton'
+import ValidationError from '../../components/ErrorHandling/ValidationError/ValidationError'
 
-function SignUpField(){
+import { mailValidator, confirmPassword } from '../../utils/validators/authFormValidators'
+
+function SignUpField() {
 
     const signUpIsOpen = useSelector(state => state.authModal.signUp)
     const dispatch = useDispatch()
-    const {register, handleSubmit, errors} = useForm()
+    const { register, handleSubmit, errors } = useForm()
+
+    // password confirmation 
+    const [password, setPassword] = useState("")
+    // const [confirm, setConfirm] = useState(initialState)
+
 
     function onSubmit(formData) {
         const fullname = formData.fullname
-        const userNickname = fullname.substring(0, fullname.indexOf(" ")) 
-        + "_" 
-        + fullname.substring(fullname.indexOf(" ") + 1, fullname.length - 1)
-        
+        const userNickname = fullname.substring(0, fullname.indexOf(" "))
+            + "_"
+            + fullname.substring(fullname.indexOf(" ") + 1, fullname.length)
+
         console.log(formData)
         dispatch(signInUser({
-            "nickname": userNickname.toLowerCase(),
-            "fullName": fullname,
-            "email": formData.email,
-            "phone": formData.phone,
-            "password": formData.password,
-            "role": 0
+            username: userNickname.toLowerCase(),
+            fullname: fullname,
+            email: formData.email,
+            phone: formData.phone,
+            password: formData.password,
+            role: 0
         }))
+        dispatch(openSignUpModal(false))
     }
 
     return (
-        <Modal className={styles.modal__style} isOpen={signUpIsOpen} onRequestClose={() => dispatch(openSignUpModal(false)) }>
+        <Modal className={styles.modal__style} isOpen={signUpIsOpen} onRequestClose={() => dispatch(openSignUpModal(false))}>
             <div className={styles.signUp__body}>
                 <h4>Qeydiyyatdan keç</h4>
-                <form 
-                    className={styles.register__form} 
+                <form
+                    className={styles.register__form}
                     onSubmit={handleSubmit(onSubmit)}
                 >
                     <label htmlFor="fullname">Ad, soyad</label>
-                    <input 
-                        id="fullname" 
-                        className={styles.long__input} 
-                        name="fullname" 
-                        placeholder="Ad Soyad" 
-                        ref={register({required: true})}/>
-                    {errors.fullname ? <p className={styles.error}>Fullname teleb olunur</p> : null}
+                    <input
+                        id="fullname"
+                        className={styles.long__input}
+                        name="fullname"
+                        placeholder="Ad Soyad"
+                        ref={register({ required: true })} />
+                    {
+                        errors.fullname
+                            ? <ValidationError errorMessage="Ad soyad teleb olunur" />
+                            : null
+                    }
 
-                    <label htmlFor="email">E-poçt ünvanı</label>
-                    <input 
-                        id="email"
-                        className={styles.long__input} 
-                        name="email" 
-                        placeholder="AdSoyad@example.com" 
-                        ref={register({required: true})}/>
-                    {errors.email ? <p className={styles.error}>Email is required</p>: null}
+                    <label htmlFor="mail">E-poçt ünvanı</label>
+                    <input
+                        id="mail"
+                        className={styles.long__input}
+                        name="email"
+                        placeholder="AdSoyad@example.com"
+                        ref={register({
+                            required: true,
+                            validate: value => mailValidator(value) === true
+                        })} />
+                    {errors.email && errors.email.type === "required"
+                        ? <ValidationError errorMessage="Email teleb olunur" />
+                        : errors.email && errors.email.type === "validate"
+                            ? <ValidationError errorMessage="Mail formatı düzgün deyil" />
+                            : null
+                    }
 
-                    <div className={styles.password__container}>  
-                       <div className={styles.password}>
+                    <div className={styles.password__container}>
+                        <div className={styles.password}>
                             <label htmlFor="psswd">Parol</label>
-                            <input  
-                                id="psswd" 
-                                className={styles.password__input} 
-                                type="password" 
+                            <input
+                                id="psswd"
+                                className={styles.password__input}
+                                type="password"
                                 name="password"
-                                placeholder="password" 
-                                ref={register({required: true})}/>
-                        </div> 
+                                placeholder="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                                ref={register({
+                                    required: true,
+                                    minLength: 8
+                                })} />
+                            {
+                                errors.password && errors.password.type === "minLength"
+                                ? <ValidationError errorMessage="minimum 8 character" />
+                                : errors.password && errors.password.type === "required"
+                                ? <ValidationError errorMessage="Parol teleb olunur" />
+                                : null
+                            }
+                        </div>
                         <div className={styles.password}>
                             <label htmlFor="psswd-confirm">Parolu tesdiq et</label>
-                            <input 
-                                id="psswd-confirm" 
-                                className={styles.password__input} 
-                                type="password" 
-                                name="password_confirm" 
-                                placeholder="password" 
-                                ref={register({required: true})}/>
+                            <input
+                                id="psswd-confirm"
+                                className={styles.password__input}
+                                type="password"
+                                name="password_confirm"
+                                placeholder="confirm password"
+                                ref={register({
+                                    required: true,
+                                    minLength: 8,
+                                    validate: value => confirmPassword(password, value)
+                                })} />
+                            {
+                                errors.password_confirm && errors.password_confirm.type === "maxLength"
+                                ? <ValidationError errorMessage="Minimum 8 character" />
+                                : errors.password_confirm && errors.password_confirm.type === "validate"
+                                ? <ValidationError errorMessage="Parol uyğun deyil" />
+                                : null
+                            }
                         </div>
                     </div>
-                    
+
                     <label htmlFor="fullname">Telefon nömrəsi</label>
-                    <input 
-                        className={styles.short__input} 
-                        name="phone" 
-                        placeholder="(___) ___ __ __" 
-                        ref={register({required: true})}/>
-                    
+                    <input
+                        className={styles.short__input}
+                        name="phone"
+                        placeholder="(___) ___ __ __"
+                        ref={register({ required: true })} />
+
                     <div className={styles.signUp__button}>
                         <SignInButton name="Qeydiyyatdan keç" />
                     </div>
                 </form>
                 <p className={styles.login__link__p}>
-                    Mövcud hesabınıza  
-                    <a onClick={() => dispatch(openLoginModal(true))}>daxil olun</a>
+                    Mövcud hesabınıza <a onClick={() => dispatch(openLoginModal(true))}>daxil olun</a>
                 </p>
             </div>
         </Modal>
