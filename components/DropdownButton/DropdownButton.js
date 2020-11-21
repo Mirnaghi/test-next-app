24afc747cde1157d71f5e245f8465663dbabf659
Admin <mirnaghi7175@gmail.com>
2020-11-21 13:40:21 +0400

    change structure of dropdown to fit items

--- components/DropdownButton/DropdownButton.js
+++ components/DropdownButton/DropdownButton.js
@@ -15,24 +15,13 @@ export default function DropdownButton({ name, values }){
             <div className={styles.dropdown__content}>
                 { values.map(value => {
                     return (
-                        <div hey={values.indexOf(value)} className={styles.dropdown__item}>
-                            <span className={styles.d__p}>{value}</span>
-                            <input className={styles.checkbox__input} type="checkbox" name="" />
+                        <div key={values.indexOf(value)} className={styles.dropdown__item}>
+                            <label for={"input"  + values.indexOf(value).toString()} className={styles.d__p}>{value}</label>
+                            <input id={"input"  + values.indexOf(value).toString()} className={styles.checkbox__input} type="checkbox" name="" />
                         </div>
                     );
                 })}
-                {/* <div className={styles.dropdown__item}>
-                    <span className={styles.d__p}>Mutleq</span>
-                    <input className={styles.checkbox__input} type="checkbox" name="" />
-                </div>
-                <div className={styles.dropdown__item}>
-                    <span className={styles.d__p}>orta</span>
-                    <input className={styles.checkbox__input} type="checkbox" name="" />
-                </div>
-                <div className={styles.dropdown__item}>
-                    <span className={styles.d__p}>vacib</span>
-                    <input className={styles.checkbox__input} type="checkbox" name="" />
-                </div> */}
+               
             </div>
         </div>
     );
