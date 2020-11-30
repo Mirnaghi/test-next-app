9eeaa87bfb98b164808477b2b86321ff4384c05e
Admin <mirnaghi7175@gmail.com>
2020-11-30 17:11:17 +0400

    modify BlueButton to add custome name and submit functionality

--- components/UI/Buttons/BlueButton/BlueButton.js
+++ components/UI/Buttons/BlueButton/BlueButton.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./BlueButton.module.css";
 
-const BlueButton = ({ name }) => {
-  return <button className={styles.BlueButton}>{name}</button>;
+const BlueButton = ({ name, onSubmit }) => {
+  return <button className={styles.BlueButton} onSubmit={onSubmit}>{name}</button>;
 };
 
 export default BlueButton;
