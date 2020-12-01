5fab0f4e99fafb7c1e8c3d0935c560824b458fa9
Admin <mirnaghi7175@gmail.com>
2020-12-01 23:34:59 +0400

    modify import in AddModuleCard

--- components/AddModuleCard/AddModuleCard.js
+++ components/AddModuleCard/AddModuleCard.js
@@ -1,6 +1,6 @@
 import styles from "./AddModuleCard.module.css";
 import {useDispatch} from "react-redux"
-import {addModuleIcon} from "../../constants";
+import {createContentIcon} from "../../constants";
 import {openCreateModule} from "../../redux/createPathModalReducer"
 import CreateModuleField from "../../containers/CreateModuleField/CreateModuleField"
 
@@ -13,7 +13,7 @@ function AddModuleCard() {
                 class={styles.module__card}
                 onClick={() => dispatch(openCreateModule())}
             >
-                {addModuleIcon}
+                {createContentIcon}
             </a>
             <CreateModuleField />
         </>
