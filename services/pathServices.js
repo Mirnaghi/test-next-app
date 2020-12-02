7f32066f92d00a1a338b1d34c999a5094a705729
Admin <mirnaghi7175@gmail.com>
2020-12-02 20:38:09 +0400

    solve typos

--- services/pathServices.js
+++ services/pathServices.js
@@ -2,15 +2,26 @@ import axios from 'axios';
 
 const baseURL = 'http://138.68.91.222';
 
-export const getPathes = async () => {
+export const getPaths = async () => {
     const response = await axios.get(`${baseURL}/api/paths/all`).then(response => {
         return response;
     }).then(value => value)
     return response.data;
 }
 
+export const createPath = async (newPath) => {
+    const response = await axios.post(
+        `${baseURL}/api/paths/new`, 
+        newPath
+    ).then( res => {
+        return res
+    }).then(result => result)
+    console.log(response)
+    return response.data
+}
+
 export const getCourses = async (id) => {
-    const courses = await axios.get(`${baseURL}/api/pathes/${id}/courses`)
+    const courses = await axios.get(`${baseURL}/api/paths/${id}/courses`)
     .then(response => response)
     .then(res => res.data)
     
