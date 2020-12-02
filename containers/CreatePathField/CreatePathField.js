25bc6a22716aa3da9fa07522ee12b545f84d9fe0
Admin <mirnaghi7175@gmail.com>
2020-12-02 20:36:10 +0400

    modify CreatePathField styling issues and add close functionality

--- containers/CreatePathField/CreatePathField.js
+++ containers/CreatePathField/CreatePathField.js
@@ -4,26 +4,52 @@ import {useSelector, useDispatch} from "react-redux"
 import {useForm} from "react-hook-form"
 import BlueButton from "../../components/UI/Buttons/BlueButton/BlueButton"
 import {openCreatePath} from "../../redux/createPathModalReducer"
+import {createNewPath} from "../../redux/createNewPathReducer"
+
 function CreatePathField() {
 
-    const open = useSelector(state => state.createPathModal.createPathIsOpen)
-    const dispatch = useDispatch()
+    const open = useSelector(state => state.createPathModal.createPathIsOpen);
+    const GUID = useSelector(state => state.userAuth.user.id)
+    const dispatch = useDispatch();
+    const {register, handleSubmit, errors} = useForm();
+
+    function onSubmit(data) {
+        console.log({
+            "name": data.pathName,
+            "description": data.pathDescription,
+            "creatorid": GUID,
+            "difficulty": 0,
+            "accessibility": 0
+        })
+        dispatch(createNewPath({
+            "name": data.pathName,
+            "description": data.pathDescription,
+            "creatorid": GUID,
+            "difficulty": 0,
+            "accessibility": 0
+        }))
+    }
 
     return (
         <Modal 
             isOpen={open}
-            onRequestClose={() => dispatch(openCreatePath)}
+            onRequestClose={() => dispatch(openCreatePath())}
             className="modal"
         >
             <div className={styles.container}>
                 <h2>Create New Path</h2>
                 <form 
                     className={styles.form__field}
+                    onSubmit={handleSubmit(onSubmit)}
                 >
                     <input type="text"
-                            placeholder="Path name"/>
+                            name="pathName"
+                            placeholder="Path name"
+                            ref={register({required: true})}/>
                     <input type="textarea"
-                            placeholder="Description"/>
+                            name="pathDescription"
+                            placeholder="Description"
+                            ref={register({required: true})}/>
                     <BlueButton name="TAMAMLA"/>
                 </form>
             </div>
