78f4c26c039c8223f0ffb03c9b5ba8ea8d72b94c
Admin <mirnaghi7175@gmail.com>
2020-12-14 03:13:48 +0400

    add spinner to CreatePathField

--- containers/CreatePathField/CreatePathField.js
+++ containers/CreatePathField/CreatePathField.js
@@ -1,26 +1,54 @@
-import styles from "./CreatePathField.module.css" 
+import styles from "./CreatePathField.module.css"
 import Modal from "react-modal"
-import {useSelector, useDispatch} from "react-redux"
-import {useForm} from "react-hook-form"
+import { useSelector, useDispatch } from "react-redux"
+import { useForm } from "react-hook-form"
 import BlueButton from "../../components/UI/Buttons/BlueButton/BlueButton"
-import {openCreatePath} from "../../redux/createPathModalReducer"
-import {createNewPath} from "../../redux/createNewPathReducer"
+import Spinner from "../../components/Spinner/Spinner"
+import { openCreatePath } from "../../redux/createPathModalReducer"
+import { createNewPath } from "../../redux/createNewPathReducer"
+import { showSpinner } from "../../redux/showSpinnerReducer"
 
 function CreatePathField() {
 
+    // state of CreatePathFiled Modal open
     const open = useSelector(state => state.createPathModal.createPathIsOpen);
-    const GUID = useSelector(state => state.userAuth.user.id)
+
+    // user`s id for createing path
+    const GUID = useSelector(state => state.userAuth.user.id);
+
+    // state of spinner
+    const spinnerIsShown = useSelector(state => state.showSpinner);
+
+    // state for unsuccessful create path
+    const success = useSelector(state => state.createPath.success);
+
+
+    // initialize hooks
     const dispatch = useDispatch();
-    const {register, handleSubmit, errors} = useForm();
+    const { register, handleSubmit, errors } = useForm();
+
 
     function onSubmit(data) {
-        console.log({
+        /*
+            function for handling form submit,
+            params: takes form data as parameter
+        */
+
+        // onSubmit show spinner
+        dispatch(showSpinner(true));
+
+        setTimeout(() => dispatch(showSpinner(false)), 3000);
+
+        // console input data for DEBUG
+        console.log("Data in path creation", {
             "name": data.pathName,
             "description": data.pathDescription,
             "creatorid": GUID,
             "difficulty": 0,
             "accessibility": 0
         })
+
+        // dispatch form data 
         dispatch(createNewPath({
             "name": data.pathName,
             "description": data.pathDescription,
@@ -28,29 +56,39 @@ function CreatePathField() {
             "difficulty": 0,
             "accessibility": 0
         }))
+
     }
 
+
     return (
-        <Modal 
+        <Modal
             isOpen={open}
             onRequestClose={() => dispatch(openCreatePath())}
             className="modal"
         >
             <div className={styles.container}>
                 <h2>Create New Path</h2>
-                <form 
+                {spinnerIsShown ? <Spinner /> : null}
+                {
+                    success === null
+                        ? null
+                        : success === true
+                            ? <p className={styles.successMessage}>Successful!</p>
+                            : <p className={styles.successMessage}>Unsuccessful!</p>
+                }
+                <form
                     className={styles.form__field}
                     onSubmit={handleSubmit(onSubmit)}
                 >
                     <input type="text"
-                            name="pathName"
-                            placeholder="Path name"
-                            ref={register({required: true})}/>
+                        name="pathName"
+                        placeholder="Path name"
+                        ref={register({ required: true })} />
                     <input type="textarea"
-                            name="pathDescription"
-                            placeholder="Description"
-                            ref={register({required: true})}/>
-                    <BlueButton name="TAMAMLA"/>
+                        name="pathDescription"
+                        placeholder="Description"
+                        ref={register({ required: true })} />
+                    <BlueButton name="TAMAMLA" />
                 </form>
             </div>
         </Modal>
