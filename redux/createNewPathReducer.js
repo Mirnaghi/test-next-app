9e582d95af243497037762d5dbffd122e0233663
Admin <mirnaghi7175@gmail.com>
2020-12-14 03:15:16 +0400

    handle unsuccessful path create

--- redux/createNewPathReducer.js
+++ redux/createNewPathReducer.js
@@ -3,20 +3,36 @@ import {createPath} from "../services/pathServices"
 export function createNewPath(newPath){
     return async (dispatch) => {
         const response = await createPath(newPath).catch(err => {
-            console.error("Path creation failed:", err)
+            console.error("Path creation failed:", err.message)
         })
         console.log("Result of createNewPath: ", response)
-        dispatch({
-            type: "CREATE_PATH",
-            payload: response
-        })
+
+        if (response !== undefined) {
+            dispatch({
+                type: "CREATE_PATH",
+                payload: response
+            })
+        } else {
+            dispatch({
+                type: "UNSUCCESSFUL_CREATE_PATH"
+            })
+        }
     }
 }
 
-export default function createNewPathReducer(state = [], action) {
+export default function createNewPathReducer(state = {newPaths: [], success: null}, action) {
     switch(action.type) {
         case "CREATE_PATH":
-            return state.concat(action.payload)
+            return {
+                ...state,
+                newPaths: state.newPath.concat(action.payload),
+                success: true
+            }
+        case "UNSUCCESSFUL_CREATE_PATH":
+            return {
+                ...state,
+                success: false
+            }   
         default:
             return state    
     }
