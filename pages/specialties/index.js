b16ce2249be79399397a9dcf0561b8e52d37df75
Admin <mirnaghi7175@gmail.com>
2020-12-16 18:39:33 +0400

    show new created paths first in paths array

--- pages/specialties/index.js
+++ pages/specialties/index.js
@@ -5,6 +5,7 @@ import MenuBar from "../../components/MenuBar/MenuBar"
 import SearchBar from "../../components/SearchBar/SearchBar"
 import SearchResult from "../../containers/SearchResult/SearchResult"
 import AddPathCard from "../../components/AddPathCard/AddPathCard" 
+import Spinner from "../../components/Spinner/Spinner"
 import {searchPath} from "../../redux/searchPathReducer"
 import {getPaths} from "../../services/pathServices"
 
@@ -12,6 +13,8 @@ export default function Specialties({allPaths}) {
   const dispatch = useDispatch()
   const searchResult = useSelector(state => state.paths.foundPaths)
   
+  // new created pathes
+  const newPaths = useSelector(state => state.createPath.newPaths)
 
   const [searching, setSearching] = useState(false)
   const [loading, setLoading] = useState(false)
@@ -37,6 +40,8 @@ export default function Specialties({allPaths}) {
     const pathes = await getPathes();
     console.log(pathes);
   }
+
+
     return (
         <div>
             <header>
@@ -47,7 +52,7 @@ export default function Specialties({allPaths}) {
                     placeholder="Xəyalndakı ixtisası daxil et (Məs: Dizayner, Developer və s)"
                     handleChange={handleInputFieldChange}
                     handleSubmit={handleSubmit} />
-                {searching ? loading ? <h1>loading...</h1> : <SearchResult searchResult={searchResult}/> : <SearchResult searchResult={allPaths}/>}
+                {searching ? loading ? <h1><Spinner /></h1> : <SearchResult searchResult={searchResult}/> : <SearchResult searchResult={newPaths.concat(allPaths)}/>}
             </div>
         </div>
     );
