8f21dd43353d4ebdc3a03fd9325c37589322bcb8
Admin <mirnaghi7175@gmail.com>
2020-12-16 18:38:26 +0400

    add textarea to CreatePathField container component and new styling

--- containers/CreatePathField/CreatePathField.js
+++ containers/CreatePathField/CreatePathField.js
@@ -5,6 +5,7 @@ import { useForm } from "react-hook-form"
 import BlueButton from "../../components/UI/Buttons/BlueButton/BlueButton"
 import Spinner from "../../components/Spinner/Spinner"
 import { openCreatePath } from "../../redux/createPathModalReducer"
+import { openLoginModal } from "../../redux/authModalReducer" 
 import { createNewPath } from "../../redux/createNewPathReducer"
 import { showSpinner } from "../../redux/showSpinnerReducer"
 
@@ -15,6 +16,9 @@ function CreatePathField() {
 
     // user`s id for createing path
     const GUID = useSelector(state => state.userAuth.user.id);
+
+    // token for createing path
+    const token = useSelector(state => state.userAuth.token);
 
     // state of spinner
     const spinnerIsShown = useSelector(state => state.showSpinner);
@@ -37,7 +41,11 @@ function CreatePathField() {
         // onSubmit show spinner
         dispatch(showSpinner(true));
 
-        setTimeout(() => dispatch(showSpinner(false)), 3000);
+        setTimeout(() => {
+            dispatch(showSpinner(false));
+            dispatch(openCreatePath());
+        }
+            , 3000);
 
         // console input data for DEBUG
         console.log("Data in path creation", {
@@ -50,11 +58,13 @@ function CreatePathField() {
 
         // dispatch form data 
         dispatch(createNewPath({
+            "token": token,
+            "newPath": {
             "name": data.pathName,
             "description": data.pathDescription,
             "creatorid": GUID,
             "difficulty": 0,
-            "accessibility": 0
+            "accessibility": 0 }
         }))
 
     }
@@ -70,12 +80,22 @@ function CreatePathField() {
                 <h2>Create New Path</h2>
                 {spinnerIsShown ? <Spinner /> : null}
                 {
-                    success === null
-                        ? null
-                        : success === true
-                            ? <p className={styles.successMessage}>Successful!</p>
-                            : <p className={styles.successMessage}>Unsuccessful!</p>
+                    success === null || token === null
+                    ? null
+                    : success === true
+                    ? <p className={styles.successMessage}>Successful!</p>
+                    : <p className={styles.unsuccessMessage}>Unsuccessful!</p>
                 }
+                {/* check user is logged in or not */}
+            {token === null 
+            ? <a 
+                className={styles.loginLink}
+                onClick={() => {
+                    dispatch(openCreatePath())
+                    dispatch(openLoginModal(true));
+                }}
+              >Please, tap to login, then create your dream path :)</a>
+            :
                 <form
                     className={styles.form__field}
                     onSubmit={handleSubmit(onSubmit)}
@@ -84,12 +104,12 @@ function CreatePathField() {
                         name="pathName"
                         placeholder="Path name"
                         ref={register({ required: true })} />
-                    <input type="textarea"
+                    <textarea
                         name="pathDescription"
-                        placeholder="Description"
+                        placeholder="Description of new path"
                         ref={register({ required: true })} />
                     <BlueButton name="TAMAMLA" />
-                </form>
+                </form>}
             </div>
         </Modal>
     );
